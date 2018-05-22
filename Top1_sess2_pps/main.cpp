0b76aca2e4f0b1d99f428ac16c248a228214f78a
cbasil98 <cbasil98@gmail.com>
2018-05-22 18:02:34 +0530

    Revert "input for 2 triangles"
    This reverts commit 043f93788452a4d6b6c659bf5de6c144c4ca1776.

--- Top1_sess2_pps/main.cpp
+++ Top1_sess2_pps/main.cpp
@@ -1,23 +1,14 @@
 #include <iostream>
 
 using namespace std;
-struct point // Data type to store a point in cartesian plane
+struct point\\ Data type to store a point in cartesian plane
 {
     int x;
     int y;
 };
-struct triangle //Data structure to store a triangle as 3 points in space
-{
-    point T[3];
-};
 
 int main()
 {
-    triangle T1,T2;
-    cout<<"First triangle";
-    for(int i=0;i<3;i++)
-            cin>>T1.T[i].x>>T1.T[i].y;
-
 
     return 0;
 }
