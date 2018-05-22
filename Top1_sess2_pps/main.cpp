77ac341f68b77f1aad0363d61cdb99957c7c72bd
cbasil98 <cbasil98@gmail.com>
2018-05-22 18:06:41 +0530

    input for triangle and point

--- Top1_sess2_pps/main.cpp
+++ Top1_sess2_pps/main.cpp
@@ -1,14 +1,30 @@
 #include <iostream>
 
 using namespace std;
-struct point\\ Data type to store a point in cartesian plane
+struct point // Data type to store a point in cartesian plane
 {
     int x;
     int y;
 };
+struct triangle //Data structure to store a triangle as 3 points in space
+{
+    point T[3];
+};
 
 int main()
 {
+    triangle T1;
+    point a;
+    cout<<"First triangle\n";
+    for(int i=0;i<3;i++)
+            cin>>T1.T[i].x>>T1.T[i].y;
+    cout<<"Insert the point";
+    cin>>a.x>>a.y;
+
+
+
+
+
 
     return 0;
 }
